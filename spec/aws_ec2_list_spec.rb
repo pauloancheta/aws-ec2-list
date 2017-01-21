dd5797714ad2f40a873d0f77728b924077edaa4f
pauloancheta <paulo.ancheta@gmail.com>
2017-01-20 17:54:24 -0800

    improve tests

--- spec/aws_ec2_list_spec.rb
+++ spec/aws_ec2_list_spec.rb
@@ -1,33 +1,23 @@
 require "spec_helper"
 require_relative 'aws_ec2_list/ec2_double'
+require_relative 'aws_ec2_list/io_double'
 
 describe AwsEc2List do
-  let(:stdout) { MyIO.new }
+  let(:stdout) { AwsEc2List::IODouble.new }
   let(:ec2) { AwsEc2List::Ec2Double }
   let(:region) { 'us-west-1' }
 
-  before { AwsEc2List.call(ec2: ec2, region: region, stdout: stdout) }
+  context "without options" do
+    before { AwsEc2List.call(ec2: ec2, region: region, stdout: stdout) }
 
-  it "has a version number" do
-    expect(AwsEc2List::VERSION).not_to be nil
-  end
+    it "has a version number" do
+      expect(AwsEc2List::VERSION).not_to be nil
+    end
 
-  it "prints out the ec2 instances available" do
-    expect(stdout.to_s).to eq %(PROJECT | STACK NAME        | PUBLIC DNS NAME | LAUNCH TIME | APP VERSION | STATE
+    it "prints out the ec2 instances available" do
+      expect(stdout.to_s).to eq %(PROJECT | STACK NAME        | PUBLIC DNS NAME | LAUNCH TIME | APP VERSION | STATE
 webapp  | lp-webapp-prod-01 | public_dns_name | launch_time | 123h12kj    | state)
+    end
   end
 end
 
-class MyIO
-  def initialize
-    @content = ""
-  end
-
-  def puts(msg)
-    @content = msg.join("\n")
-  end
-
-  def to_s
-    @content
-  end
-end
