a12e0947eecd7f01f1bde61827b4f5c0bf015bba
pauloancheta <paulo.ancheta@gmail.com>
2017-02-02 17:53:08 -0800

    remove comment

--- lib/aws_ec2_list/runner.rb
+++ lib/aws_ec2_list/runner.rb
@@ -20,36 +20,6 @@ class AwsEc2List::Runner
   private
 
   def instances
-
-    #  Aws::EC2::Types::Instance
-    #  instance_id="i-0e5846a2b3988300d",
-    #  state=#<struct Aws::EC2::Types::InstanceState code=16, name="running">,
-    #  private_dns_name="ip-172-16-92-73.ec2.internal",
-    #  public_dns_name="ec2-54-157-47-22.compute-1.amazonaws.com",
-    #  instance_type="m4.xlarge",
-    #  launch_time=2017-01-12 19:46:29 UTC,
-    #  subnet_id="subnet-844ef9df",
-    #  vpc_id="vpc-2d583b48",
-    #  private_ip_address="172.16.92.73",
-    #  public_ip_address="54.157.47.22",
-    #  architecture="x86_64",
-    #  client_token="40459b52-177d-4e54-a3ed-e07e9d434ecc_subnet-844ef9df_5",
-    #  tags=[
-    #    #<struct Aws::EC2::Types::Tag key="Name", value="lp-webapp-product-0e5846a2b3988300d">,
-    #    #<struct Aws::EC2::Types::Tag key="aws:cloudformation:logical-id", value="WebASG">,
-    #    #<struct Aws::EC2::Types::Tag key="owner", value="a-team">,
-    #    #<struct Aws::EC2::Types::Tag key="environment", value="production">,
-    #    #<struct Aws::EC2::Types::Tag key="aws:autoscaling:groupName", value="lp-webapp-production-31-WebASG-RYQPV710NC8B">,
-    #    #<struct Aws::EC2::Types::Tag key="aws:cloudformation:stack-name", value="lp-webapp-production-31">,
-    #    #<struct Aws::EC2::Types::Tag key="app_version", value="9da8527">,
-    #    #<struct Aws::EC2::Types::Tag key="aws:cloudformation:stack-id", value="arn:aws:cloudformation:us-east-1:002682819933:stack/lp-webapp-production-31/09547990-d8ff-11e6-aedf-50fae9882035">,
-    #    #<struct Aws::EC2::Types::Tag key="datadog", value="true">,
-    #    #<struct Aws::EC2::Types::Tag key="project", value="lp-webapp">
-    #  ],
-    #  security_groups=[
-    #    #<struct Aws::EC2::Types::GroupIdentifier group_name="vpc-us-east-classic-link-ClassicLinkSecurityGroup-1H5UKPIA5IANJ", group_id="sg-87c20ce3">,
-    #    #<struct Aws::EC2::Types::GroupIdentifier group_name="shared-security-groups-mainvpc-SshSecurityGroup-15DV69GVO6M5F", group_id="sg-c5ba0db8">
-    #  ]
     reservations.map(&:instances).flatten.map do |instance|
       AwsEc2List::Instance.new(
         instance_id: instance.instance_id,
